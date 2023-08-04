26371834c65af4a6b69b5377d5263d3d836902e7
Muhammed Ali Güzel <76560828+MuhammedGzel@users.noreply.github.com>
2023-08-04 16:43:51 +0300

    Add files via upload

---
+++ StackClass.java
@@ -0,0 +1,48 @@
+
+package yıgıtapplet;
+
+
+public class StackClass {
+    int[] array;
+    int top;
+    int size;
+
+    public StackClass(int size) {
+        this.size=size;
+        this.array=new int[size];
+        this.top=-1;
+    }
+    
+    public boolean isFull(){
+        if(top==this.size-1){
+            return true;}
+        else{
+            return false;}
+                           }
+    public boolean isEmpty(){
+        if(top==-1){
+            return true;}
+        else{
+            return false;}
+                          }
+    public int pop(){
+        if(top!=-1){
+          return array[top--];  
+        }
+        else{
+            return -1;
+        }
+    }
+    public void push(int key){
+        if(!isFull()){
+            array[++top]=key;
+        }
+        else{
+            System.out.println("Stack is full");
+        }
+    }
+    public int returnTop(){
+        return array[top];
+    }
+    
+}
