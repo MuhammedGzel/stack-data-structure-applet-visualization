aace90b53704344a6dfa623be2db66521ff15c88
Muhammed Ali Güzel <76560828+MuhammedGzel@users.noreply.github.com>
2023-08-04 17:20:59 +0300

    Add files via upload

--- src/stackapplet/StackClass.java
+++ src/stackapplet/StackClass.java
@@ -1,48 +1,51 @@
 
 package stackapplet;
 
-
 public class StackClass {
     int[] array;
     int top;
     int size;
 
     public StackClass(int size) {
-        this.size=size;
-        this.array=new int[size];
-        this.top=-1;
+        this.size = size;
+        this.array = new int[size];
+        this.top = -1;
     }
-    
-    public boolean isFull(){
-        if(top==this.size-1){
-            return true;}
-        else{
-            return false;}
-                           }
-    public boolean isEmpty(){
-        if(top==-1){
-            return true;}
-        else{
-            return false;}
-                          }
-    public int pop(){
-        if(top!=-1){
-          return array[top--];  
+
+    public boolean isFull() {
+        if (top == this.size - 1) {
+            return true;
+        } else {
+            return false;
         }
-        else{
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int pop() {
+        if (top != -1) {
+            return array[top--];
+        } else {
             return -1;
         }
     }
-    public void push(int key){
-        if(!isFull()){
-            array[++top]=key;
-        }
-        else{
+
+    public void push(int key) {
+        if (!isFull()) {
+            array[++top] = key;
+        } else {
             System.out.println("Stack is full");
         }
     }
-    public int returnTop(){
+
+    public int returnTop() {
         return array[top];
     }
-    
+
 }
